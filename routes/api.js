f83105d3130ee3e0ee08ebff7f7faa4f52ef6b0d
Delvoid <delvoidltd@gmail.com>
2021-11-06 19:28:18 +0000

    db setup, mongoose added, nodemon added, post endpoint, get endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,60 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
-
-'use strict';
+'use strict'
+const BookModel = require('../models/BookModel')
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route('/api/books')
+    .get(async (req, res) => {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      const books = await BookModel.find({})
+      if (!books) return res.json([])
+      const bookFormat = books.map((book) => {
+        return {
+          _id: book._id,
+          title: book.title,
+          commentcount: book.comments.length,
+        }
+      })
+      res.json(bookFormat)
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
+
+    .post(async (req, res) => {
       //response will contain new book object including atleast _id and title
+      const title = req.body.title
+      if (!title) return res.send('missing required field title')
+      try {
+        const newBook = new BookModel({
+          title,
+          comments: [],
+        })
+        const savedBook = await newBook.save()
+        res.json({ _id: savedBook.id, title: savedBook.title })
+      } catch (error) {
+        console.log(error)
+        res.send('error saving')
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
-    });
+    })
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
+  app
+    .route('/api/books/:id')
+    .get(function (req, res) {
+      let bookid = req.params.id
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
+
+    .post(function (req, res) {
+      let bookid = req.params.id
+      let comment = req.body.comment
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
+
+    .delete(function (req, res) {
+      let bookid = req.params.id
       //if successful response will be 'delete successful'
-    });
-  
-};
+    })
+}
